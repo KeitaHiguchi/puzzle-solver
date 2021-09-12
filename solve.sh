5d0815e156e4b7a38ab4dfb60305ab823de64865
Keita Higuchi <blanc0000002@gmail.com>
2021-09-12 22:48:09 +0900

    add: searchoffset function

--- solve.sh
+++ solve.sh
@@ -4,6 +4,7 @@
 # 指定位置にピースを置く.
 # Globals:
 #   frame - 現在の盤面ファイル
+#   nextFrame - 結果の盤面ファイル
 # Arguments:
 #   $1 - 開始行番号
 #   $2 - 開始カラム番号
@@ -34,14 +35,37 @@ function setPiece() {
     }' > nextFrame
 }
 
+function hasError() {
+    [ $(grep -c "[2|5]" nextFrame) -gt 0 ]
+}
+
 function createFrame() {
     # stub
     cat frame.txt > frame
 }
 
+function getOffsets() {
+    local file=$1
+    matchedRow=$(grep -m 1 -n 1 $file)
+    offsetRow=$(echo $matchedRow | sed 's/:.*//g')
+    offsetCol=$(($(echo $matchedRow | sed 's/[0-9]\+://g' | grep -oP "^.*?1" | wc -c) / 2))
+
+    echo $file
+    echo matchrow : $matchedRow
+    echo offsetRow : $offsetRow
+    echo offsetCol :  $offsetCol
+}
+
 function main() {
     createFrame
     setPiece 2 3 "piece/1/1.txt"
+    if [ $(hasError) ] ; then
+        echo era-gaarimasu!
+    else
+        echo moumantai! 
+        cat nextFrame > frame
+        getOffsets frame
+    fi
 }
 
 main
